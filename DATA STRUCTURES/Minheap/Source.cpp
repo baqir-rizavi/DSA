a1a78eb371f51728f39f94ccd00233bb86d5cccc
Baqir Rizavi <70775533+baqir-rizavi@users.noreply.github.com>
2021-06-28 14:53:24 +0500

    Add files via upload

--- DATA STRUCTURES/Minheap/Source.cpp
+++ DATA STRUCTURES/Minheap/Source.cpp
@@ -20,16 +20,16 @@ int main()
 {
 	MinHeap<int> h;
 	h.insert(6);
+	h.insert(13);
 	h.insert(4);
 	h.insert(8);
+	h.insert(14);
 	h.insert(3);
 	h.insert(9);
-	h.insert(13);
 	h.insert(2);
 	h.insert(5);
-	h.insert(10);
 	h.insert(11);
-	h.insert(14);
+	h.insert(10);
 	h.insert(1);
 	h.insert(12);
 	int n = 0;
@@ -72,22 +72,46 @@ T MinHeap<T>::removeMin()
 	T retVal = data[0];
 	data[0] = data[nodesCount - 1];
 	int i = 0;
-	while ((i < ((nodesCount - 1) / 2)) && (data[i] > data[2 * i + 1] || data[i] > data[2 * i + 2]))
+	bool isSwapped = true;
+	while (i <= ((nodesCount - 1) / 2) && isSwapped) // && (2 * i + 1 < nodesCount || 2 * i + 2 < nodesCount) && (data[i] > data[2 * i + 1] || data[i] > data[2 * i + 2])
 	{
-		if ((data[i] > data[2 * i + 1]) && (data[i] > data[2 * i + 2]))
+		isSwapped = false;
+		if (2 * i + 1 < nodesCount)
 		{
-			swap(data[i], (data[2 * i + 1] <= data[2 * i + 2] ? data[2 * i + 1] : data[2 * i + 2]));
-			i = (data[2 * i + 1] <= data[2 * i + 2] ? (2 * i + 1) : (2 * i + 2));
+			if (2 * i + 2 < nodesCount)
+			{
+				if (data[i] > data[2 * i + 1] && data[i] > data[2 * i + 2])
+				{
+					swap(data[i], (data[2 * i + 1] <= data[2 * i + 2] ? data[2 * i + 1] : data[2 * i + 2]));
+					i = (data[2 * i + 1] <= data[2 * i + 2] ? (2 * i + 1) : (2 * i + 2));
+					isSwapped = true;
+				}
+				else if (data[i] > data[2 * i + 1])
+				{
+					swap(data[i], data[2 * i + 1]);
+					i = i * 2 + 1;
+					isSwapped = true;
+				}
+				else if (data[i] > data[2 * i + 2])
+				{
+					swap(data[i], data[2 * i + 2]);
+					i = i * 2 + 2;
+					isSwapped = true;
+				}
+			}
+			else if (data[i] > data[2 * i + 1])
+			{
+				swap(data[i], data[2 * i + 1]);
+				i = i * 2 + 1;
+				isSwapped = true;
+			}
+			
 		}
-		else if (data[i] > data[2 * i + 1])
-		{
-			swap(data[i], data[2 * i + 1]);
-			i = i * 2 + 1;
-		}
-		else //if (data[i] > data[2 * i + 2])
+		else if (data[i] > data[2 * i + 2])
 		{
 			swap(data[i], data[2 * i + 2]);
 			i = i * 2 + 2;
+			isSwapped = true;
 		}
 	}
 	nodesCount--;
